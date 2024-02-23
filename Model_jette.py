ed047473435eec2bd069e77048fa4a7fe2562425
098372 <r098372@rad-hpc-master-001.cluster>
2024-02-23 12:32:57 +0100

    Working on model - feb 23

--- Model_jette.py
+++ Model_jette.py
@@ -10,8 +10,12 @@ import pandas as pd
 import h5py
 from sklearn.preprocessing import StandardScaler
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from lifelines.utils import concordance_index
 
 from monai.networks.nets import FullyConnectedNet
+
+import mlflow
 # --------------------------------------------------------------------------------------------------------
 
 # Now writing it without functions (to test), later write this into functions as part of a class
@@ -63,10 +67,7 @@ print(train_df)
 print(test_df)
 
 # # Step 2d: Split into train and test set, but we do need to seperate the data (covariates) and corresponding labels (event, duration)
-# X_train = train_df.drop(['y', 'e'], axis=1)
-# y_train = train_df[['y', 'e']]
-# X_test = test_df.drop(['y', 'e'], axis=1)
-# y_test = test_df[['y', 'e']]
+# I should also create a train and test dataloader and convert the data to a tensor to actually use it in the model
 
 class SurvivalDataset(Dataset):
     def __init__(self, dataframe):
@@ -85,7 +86,7 @@ train_dataset = SurvivalDataset(train_df)
 test_dataset = SurvivalDataset(test_df)
 
 # Create custom dataloaders
-batch_size = 32  # You can adjust this value based on your memory constraints
+batch_size = 32  
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
 
@@ -93,7 +94,7 @@ test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
 
 # Step 3 Build a model
 # Define parameters
-in_channels = X_train.shape[1]      # Number of input channels
+in_channels = x_train.shape[1]      # Number of input channels
 out_channels = 1                    # Numer of output channels (1 for survival analysis)
 hidden_channels = [10, 10, 10]      # Number of output channels of each hidden layer
 dropout = 0.1                       
@@ -105,6 +106,15 @@ class Survivalmodel(pl.LightningModule):
         # We use the FullyConnectedNet as a model to replicate DeepSurv
         self.model = FullyConnectedNet(in_channels=in_channels, out_channels=1, hidden_channels=hidden_channels, dropout=dropout)
 
+        # We want to log everything
+        mlflow.start_run()
+        mlflow.log_params({
+            'in_channels': in_channels,
+            'out_channels': out_channels,
+            'hidden_channels': hidden_channels,
+            'dropout': dropout
+        })
+
     def forward(self, x):
         return self.model(x)
 
@@ -112,7 +122,6 @@ class Survivalmodel(pl.LightningModule):
         optimizer = torch.optim.SGD(self.parameters(), lr=0.01)
         return optimizer
 
-# Do I even have the right input? risk_pred, y, e ???? I think y is duration and e is event in my dataset
     def loss_fn(self, risk_pred, y, e, l2reg):
         mask = torch.ones(y.shape[0], y.shape[0])
         mask[(y.T - y) > 0] = 0
@@ -128,19 +137,54 @@ class Survivalmodel(pl.LightningModule):
             return neg_log_loss + l2_loss
         else:
             return neg_log_loss
+    
+    def c_index(self, risk_pred, y, e):
+        '''
+        We want to check whether the inputs are numpy arrays (this is expected in the function concordance_index).
+        If not, we have to convert them to numpy arrays. Then we can use the imported function to calculate the c-index
+        '''
+        if not isinstance(y, np.ndarray):
+            y = y.detach().cpu().numpy()
+        if not isinstance(risk_pred, np.ndarray):
+            risk_pred = risk_pred.detach().cpu().numpy()
+        if not isinstance(e, np.ndarray):
+            e = e.detach().cpu().numpy()
+        return concordance_index(y, risk_pred, e) # Maybe risk_pred should be negative sign
 
     def training_step(self, batch, batch_idx):
         x, y, e = batch['x'], batch['y'][:, 0], batch['y'][:, 1]
         risk_pred = self.forward(x)
         loss = self.loss_fn(risk_pred, y, e, l2reg)
-        return {'loss:' loss}
+        c_index_value = self.c_index(risk_pred, y, e)
+
+        # Logging with MLflow
+        mlflow.log_metric('train_loss', loss.item())
+        mlflow.log_metric('train_c_index', c_index_value)
+        return {'loss': loss}
 
 
-    def validation_step(self, val_batch, batch_idx):
+    def validation_step(self, batch, batch_idx):
         x, y, e = batch['x'], batch['y'][:, 0], batch['y'][:, 1]
         risk_pred = self.forward(x)
         loss = self.loss_fn(risk_pred, y, e, l2reg)
-        return {'loss:' loss}
+        c_index_value = self.c_index(risk_pred, y, e)
+
+        # Logging with MLflow
+        mlflow.log_metric('val_loss', loss.item())
+        mlflow.log_metric('val_c_index', c_index_value)
+        return {'loss': loss}
+
+    def on_train_end(self):
+        mlflow.end_run()
+
+# Now lets try to actually train my model
+max_epochs = 100
+model = Survivalmodel(in_channels=in_channels, out_channels=1, hidden_channels=hidden_channels, dropout=dropout)
+trainer = pl.Trainer(max_epochs=max_epochs)
+
+trainer.fit(model,train_dataloaders=train_dataloder, val_dataloaders=val_dataloader)
+
+
 
 
 
